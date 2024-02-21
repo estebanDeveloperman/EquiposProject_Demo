d6d8b616fbb8ce38eef10117bee655108370dfc0
estebanDeveloperman <estebandesarrolladordesoftware@gmail.com>
2024-02-21 01:24:25 -0500

    un pequeno cambio para produccion

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -26,7 +26,7 @@ const Add = () => {
     e.preventDefault();
     try {
       // await axios.post("http://localhost:8800/equipos", equipo);
-      await axios.post("https://equipoprojectbackend-production.up.railway.app//equipos", equipo);
+      await axios.post("https://equipoprojectbackend-production.up.railway.app/equipos", equipo);
       navigate("/");
     } catch (err) {
       console.log(err);
